7d16c492402681bcbfed47fef546273e6a6bcc91
osmar <osmar.torres@uabc.edu.mx>
2024-06-25 12:39:47 -0700

    GUI agregada

---
+++ MineScape/scenes/GameOverScreen/GameOverV2.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_button_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/MainMenu/MainMenu.tscn")
+
+func _on_button_quit_pressed():
+	get_tree().change_scene_to_file("res://Scenes/level_base/LevelBase.tscn")
